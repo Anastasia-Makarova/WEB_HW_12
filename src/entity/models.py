eb7732c8cbff14bc64b1f11255f60be751b099ac
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-02-21 11:28:51 +0100

    1

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import String, Date
-from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
+from sqlalchemy import String, Date, Integer, ForeignKey, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -17,3 +18,19 @@ class Contact(Base):
     email: Mapped[str] = mapped_column(String(40))
     birthday: Mapped[str] = mapped_column(Date)
     notes: Mapped[str] = mapped_column(String(200))
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now(), nullable=True)
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now(), nullable=True)
+    user_id: Mapped[int] = mapped_column (Integer, ForeignKey('users.id'), nullable=True)
+    user: Mapped['User'] = relationship('User', backref='contacts', lazy='joined' )
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now())
---
+++ src/routres/auth.py
@@ -0,0 +1,58 @@
+from datetime import date
+
+from fastapi import APIRouter, HTTPException, Depends, Security, status, Path, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from src.database.db import get_db
+from src.repository import users as repository_users
+from src.schemas.user  import UserSchema, UserResponse, TokenSchema
+
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+@router.post("/signup")
+async def signup(body: UserModel, db: AsyncSession = Depends(get_db)):
+    # exist_user = db.query(User).filter(User.email == body.username).first()
+    # if exist_user:
+    #     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
+    # new_user = User(email=body.username, password=hash_handler.get_password_hash(body.password))
+    # db.add(new_user)
+    # db.commit()
+    # db.refresh(new_user)
+    pass
+    return {}
+
+
+@router.post("/login")
+async def login(body: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    # user = db.query(User).filter(User.email == body.username).first()
+    # if user is None:
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
+    # if not hash_handler.verify_password(body.password, user.password):
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
+    # # Generate JWT
+    # access_token = await create_access_token(data={"sub": user.email})
+    # refresh_token = await create_refresh_token(data={"sub": user.email})
+    # user.refresh_token = refresh_token
+    # db.commit()
+    pass
+    return {}
+
+
+@router.get('/refresh_token')
+async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security), db: AsyncSession = Depends(get_db)):
+    # token = credentials.credentials
+    # email = await get_email_form_refresh_token(token)
+    # user = db.query(User).filter(User.email == email).first()
+    # if user.refresh_token != token:
+    #     user.refresh_token = None
+    #     db.commit()
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
+
+    # access_token = await create_access_token(data={"sub": email})
+    # refresh_token = await create_refresh_token(data={"sub": email})
+    # user.refresh_token = refresh_token
+    # db.commit()
+    pass
+    return {}
