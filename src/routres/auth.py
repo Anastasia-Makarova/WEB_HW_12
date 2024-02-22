aa43e8884bfeafc890134bc02c858296ef0785d4
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-02-22 10:38:38 +0100

    login, refresh_token

--- src/routres/auth.py
+++ src/routres/auth.py
@@ -24,20 +24,18 @@ async def signup(body: UserSchema, db: AsyncSession = Depends(get_db)):
 
 
 
-@router.post("/login")
+@router.post("/login", response_model=TokenSchema)
 async def login(body: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
-    # user = db.query(User).filter(User.email == body.username).first()
-    # if user is None:
-    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
-    # if not hash_handler.verify_password(body.password, user.password):
-    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
-    # # Generate JWT
-    # access_token = await create_access_token(data={"sub": user.email})
-    # refresh_token = await create_refresh_token(data={"sub": user.email})
-    # user.refresh_token = refresh_token
-    # db.commit()
-    pass
-    return {}
+    user = await repository_users.get_user_by_email(body.username, db)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
+    if not auth_service.verify_password(body.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
+    # Generate JWT
+    access_token = await auth_service.create_access_token(data={"sub": user.email})
+    refresh_token = await auth_service.create_refresh_token(data={"sub": user.email})
+    await repository_users.update_token(user, refresh_token, db)
+    return {'access_token': access_token, 'refresh_token': refresh_token, 'token_type': 'bearer'}
 
 
 @router.get('/refresh_token')
